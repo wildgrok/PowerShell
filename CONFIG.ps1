321a65a63604b7921a7fa442c378a1ab6b1dc3fe
wildgrok <wildgrok@users.noreply.github.com>
2016-03-18 16:50:19 -0400

    Configuration file

---
+++ CONFIG.ps1
@@ -0,0 +1,5 @@
+$global:SQLSTORAGESTRING 	= ''
+$global:LOGSQLFILE 			= 'SQLLOG.SQL'
+$global:ACTIONSFILE 		= 'ACTIONS.DAT'
+$global:DBLIST 				= 'DBLIST_ACTIONS_LATEST.CSV'
+$global:MAXTHREADS 			= 0
