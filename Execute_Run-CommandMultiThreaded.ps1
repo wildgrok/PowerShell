d5401168cfc236276fd034028345958b25fffd9f
wildgrok <wildgrok@users.noreply.github.com>
2016-03-18 16:42:37 -0400

    Update Execute_Run-CommandMultiThreaded.ps1
    Uses Run-CommandMultiThreaded.ps1 from Ryan Witschger - http://www.Get-Blog.com

--- Execute_Run-CommandMultiThreaded.ps1
+++ Execute_Run-CommandMultiThreaded.ps1
@@ -1,20 +1,19 @@
 # Execute_Run-CommandMultiThreaded.ps1
-#Last updated
-#7/1/2015:  modified to use new CONFIG.ps1 file
+
 
 # Calls Run-CommandMultiThreaded.ps1
-# Expects parameters DBLIST and MaxThreads (set in CONFIG.ps1 file)
+# Expects parameters DBLIST and MaxThreads 
 # If MaxThreads is 0 it will use the total number of lines enabled in the text file
 
 # Import globals
-Set-Location 'D:\Users\jorgebe\Documents\powershell'
-#. .\CONFIG.ps1
+Set-Location 'D:\WORKFOLDER'
+
 
 $global:SQLSTORAGESTRING 	= ''
-$global:LOGSQLFILE 			= 'SQLLOG.SQL'
+$global:LOGSQLFILE 		= 'SQLLOG.SQL'
 $global:ACTIONSFILE 		= 'ACTIONS.DAT'
-$global:DBLIST 				= 'DBLIST_ACTIONS_LATEST.CSV'
-$global:MAXTHREADS 			= 0
+$global:DBLIST 			= 'DBLIST_ACTIONS_LATEST.CSV'
+$global:MAXTHREADS 		= 0
 
 
 # used for automatic thread option: 
@@ -42,7 +41,4 @@ Write-Host "Using " $global:MAXTHREADS " threads"
 
 # Run the program
 gc $global:DBLIST | .\Run-CommandMultiThreaded.ps1 -Command .\ItemActions.ps1 -MaxThreads $global:MAXTHREADS
-#gc $global:DBLIST | .\Run-CommandMultiThreaded.ps1 -Command .\ItemActions_WORKFLOW.ps1 -MaxThreads $global:MAXTHREADS
-#gc $global:DBLIST | .\Run-CommandMultiThreaded.ps1 -Command .\ItemActions_DEMO.ps1 -MaxThreads $global:MAXTHREADS
 
-#$args
