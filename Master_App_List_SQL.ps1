4f7b287e59f6d1ca619ef6a2d0f50250177dbec6
wildgrok <wildgrok@users.noreply.github.com>
2019-02-13 13:39:36 -0500

    Add files via upload
    Sample jobs script

---
+++ Master_App_List_SQL.ps1
@@ -0,0 +1,303 @@
+#version in \\ccluatsql1\OPCONJOBS\MasterApplicationList
+$SQL_Reload_EnvironmentsAndApplications = @"
+	truncate table [Master_Application_List].[dbo].[Servers and Databases]
+	truncate table [Master_Application_List].[dbo].[Environments And Applications]
+	INSERT INTO [Master_Application_List].[dbo].[Environments And Applications]
+	(
+	[Application_Name]
+	      ,[Application Owner]
+		  ,[Application_Tech]
+	      ,[Environment]
+	      ,[Server]
+	      ,[Database]
+	      ,[ShoreSide_Shipboard]
+	      ,[SqlVersion]
+	      ,[Online_Offline]
+	)
+	SELECT distinct
+	a.[Application_Name]
+	,a.[Application Owner]
+	,a.[Application_Tech]
+	,a.[Environment]
+	,a.[Server]
+	,a.[Database]
+	,a.[ShoreSide_Shipboard]
+	,a.[SqlVersion]	 
+	,a.[Online_Offline]
+	FROM [Master_Application_List].[dbo].[Environments And Applications BACKUP] a
+	  order by a.[Environment] ,a.[Server]
+"@
+
+$SQL_GetSQLServers = 'select SqlServer from [Master_Application_List].[dbo].[VW_SQLSERVERS] order by SqlServer'
+$SQL_GetServers    = 'select Machine from [Master_Application_List].[dbo].[VW_SERVERS] order by Machine'
+$SQL_Get_DBSFromServer = 'set nocount on select name, state_desc as Online_Offline, @@version as SqlVersion from sys.databases where database_id > 4 order by name'
+$SQL_GetMissingFrom_ServersAndDatabases = 
+@"
+SET NOCOUNT ON
+INSERT INTO [Master_Application_List].[dbo].[Environments And Applications]
+           ([Application_Name]
+           ,[Application Owner]
+		   ,[Application_Tech]
+           ,[Environment]
+           ,[Server]
+           ,[Database]
+           ,[ShoreSide_Shipboard]
+           ,[SqlVersion]
+           ,[Online_Offline]
+)
+SELECT 
+b.[Application_Name]
+,b.[Application Owner]
+,b.[Application_Tech]
+,b.[Environment]
+,a.[SQLServer]
+,a.[Database_Name]
+,b.[ShoreSide_Shipboard]
+,a.[SqlVersion]	 
+,a.[Online_Offline]
+  FROM [Master_Application_List].[dbo].[Environments And Applications] b  
+  RIGHT JOIN  [Master_Application_List].[dbo].[Servers and Databases] a on 
+  a.SQLServer = b.[Server] AND
+  a.Database_Name = b.[Database]
+  where b.[Server] is null and 
+  b.[Database] is null
+"@
+
+$SQL_FixVersionsAndOnlineOffline = 
+@"
+SET NOCOUNT ON
+UPDATE u
+   SET 
+      u.[SqlVersion] = s.sqlversion
+	  from [Master_Application_List].[dbo].[Environments And Applications] u
+ JOIN [Master_Application_List].[dbo].[Servers and Databases] s on
+	u.[Server] = s.[SqlServer]
+UPDATE u
+   SET 
+      u.[SqlVersion] = s.sqlversion
+	  from [Master_Application_List].[dbo].[Environments And Applications BACKUP] u
+ JOIN [Master_Application_List].[dbo].[Servers and Databases] s on
+	u.[Server] = s.[SqlServer]
+	
+UPDATE u
+   SET 
+      u.[Online_Offline] = s.[Online_Offline]
+	  from [Master_Application_List].[dbo].[Environments And Applications] u
+ JOIN [Master_Application_List].[dbo].[Servers and Databases] s on
+	u.[Server] = s.[SqlServer]  and
+	u.[Database] = s.Database_Name
+	
+UPDATE u
+   SET 
+      u.[Online_Offline] = s.[Online_Offline]
+	  from [Master_Application_List].[dbo].[Environments And Applications BACKUP] u
+ JOIN [Master_Application_List].[dbo].[Servers and Databases] s on
+	u.[Server] = s.[SqlServer]  and
+	u.[Database] = s.Database_Name			
+"@
+
+#Enabled 9/1/2016
+$SQL_UpdateFrom_EnvironmentsAndApplicationsBACKUP = 
+@"
+SET NOCOUNT ON
+UPDATE u
+   SET u.[Application_Name] = s.[Application_Name]
+      ,u.[Application Owner] = s.[Application Owner]
+	  ,u.[Application_Tech] = s.[Application_Tech] 
+      ,u.[Environment] = s.[Environment]
+      --,u.[Server] = s.[Server]
+      --,u.[Database] = <Database, varchar(100),>
+      ,u.[ShoreSide_Shipboard] = s.[ShoreSide_Shipboard]
+      --,u.[SqlVersion] = s.[SqlVersion] 
+      --,u.[Online_Offline] = s.[Online_Offline] 
+      --,u.[LastUpdated] = <LastUpdated, datetime,>
+
+FROM [Master_Application_List].[dbo].[Environments And Applications] u
+RIGHT JOIN [Master_Application_List].[dbo].[Environments And Applications BACKUP] s on
+	u.[Server] = s.[Server] AND
+	u.[Database] = s.[Database]
+"@
+
+# added 8/24/2017
+$SQL_GetLogins =
+@"
+select name, dbname, sysadmin, isntgroup, isntuser
+from master.dbo.syslogins where left(name, 2) <> '##' 
+order by name, dbname
+"@
+
+# added 8/25/2017
+$SQL_GetUsers = 
+@"
+set nocount on
+DECLARE @name sysname,
+@sql nvarchar(4000),
+@maxlen1 smallint,
+@maxlen2 smallint,
+@maxlen3 smallint
+
+IF EXISTS (SELECT TABLE_NAME FROM tempdb.INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME LIKE '#tmpTable%')
+DROP TABLE #tmpTable
+
+CREATE TABLE #tmpTable 
+(
+DBName sysname NOT NULL ,
+UserName sysname NOT NULL,
+RoleName sysname NOT NULL
+)
+
+DECLARE c1 CURSOR for 
+SELECT name FROM master.sys.databases where state_desc = 'ONLINE'
+
+OPEN c1
+FETCH c1 INTO @name
+WHILE @@FETCH_STATUS >= 0
+BEGIN
+SELECT @sql = 
+'INSERT INTO #tmpTable
+SELECT N'''+ @name + ''', a.name, c.name
+FROM [' + @name + '].sys.database_principals a 
+JOIN [' + @name + '].sys.database_role_members b ON b.member_principal_id = a.principal_id
+JOIN [' + @name + '].sys.database_principals c ON c.principal_id = b.role_principal_id
+WHERE a.name != ''dbo'''
+EXECUTE (@sql)
+FETCH c1 INTO @name
+END
+CLOSE c1
+DEALLOCATE c1
+
+SELECT @maxlen1 = (MAX(LEN(COALESCE(DBName, 'NULL'))) + 2)
+FROM #tmpTable
+
+SELECT @maxlen2 = (MAX(LEN(COALESCE(UserName, 'NULL'))) + 2)
+FROM #tmpTable
+
+SELECT @maxlen3 = (MAX(LEN(COALESCE(RoleName, 'NULL'))) + 2)
+FROM #tmpTable
+
+SET @sql = 'SELECT LEFT(DBName, ' + LTRIM(STR(@maxlen1)) + ') AS ''DBName'', '
+SET @sql = @sql + 'LEFT(UserName, ' + LTRIM(STR(@maxlen2)) + ') AS ''UserName'', '
+SET @sql = @sql + 'LEFT(RoleName, ' + LTRIM(STR(@maxlen3)) + ') AS ''RoleName'' '
+SET @sql = @sql + 'FROM #tmpTable '
+SET @sql = @sql + 'WHERE Left(UserName, 2) <> ''##'' '
+SET @sql = @sql + 'ORDER BY DBName, UserName'
+EXEC(@sql)
+"@
+
+
+$SQL_GetBackupInfo = 
+@"
+SET NOCOUNT ON
+create table #temptemp
+(
+	db varchar(100), 
+	bkdate datetime
+)
+insert into #temptemp
+SELECT 
+   msdb.dbo.backupset.database_name, 
+   MAX(msdb.dbo.backupset.backup_finish_date) AS last_db_backup_date
+ FROM   msdb.dbo.backupmediafamily 
+   INNER JOIN msdb.dbo.backupset ON msdb.dbo.backupmediafamily.media_set_id = msdb.dbo.backupset.media_set_id 
+WHERE  msdb..backupset.type = 'D'
+GROUP BY
+   msdb.dbo.backupset.database_name 
+ORDER BY 
+   last_db_backup_date desc
+
+select 
+rtrim(db) as 'db', 
+bkdate from #temptemp 
+where bkdate < getdate() - 1 
+order by db
+"@
+
+$SQL_ClearMissingBackupsTable = "truncate table [Master_Application_List].[dbo].[Missing Backups]"
+$SQL_InsertIntoBackupHistory = 
+@"
+SET NOCOUNT ON
+INSERT INTO [Master_Application_List].[dbo].[Missing Backups History]
+(
+	[SqlServer]
+    ,[DBname]
+    ,[BKDate]
+)
+select * FROM [Master_Application_List].[dbo].[Missing Backups]
+delete from [Master_Application_List].[dbo].[Missing Backups History]
+where rundate < (getdate() - 30)
+"@
+
+$SQL_Insert_Production_Values = 
+@"
+INSERT INTO [Master_Application_List].[dbo].[Environments And Applications]
+(
+	[Application_Name]
+   ,[Application Owner]
+   ,[Application_Tech]
+   ,[Environment]
+   ,[Server]
+   ,[Database]
+   ,[ShoreSide_Shipboard]
+   ,[SqlVersion]
+   ,[Online_Offline]		   
+)
+	SELECT distinct
+     [Application_Name]
+     ,[Application Owner]
+     ,[Application_Tech]
+     ,[Environment]
+     ,[Server]
+	 ,[Database] 
+	 ,[ShoreSide_Shipboard]
+     ,[SqlVersion]
+     ,[Online_Offline]
+	FROM [Master_Application_List].[dbo].[Environments And Applications BACKUP]
+	WHERE [Server] like '%PRD%'
+"@
+
+$SQL_Servers_And_Databases_Compatibility_Differences_Query = 
+@"
+set nocount on
+select
+cast(serverproperty('productversion') as varchar(100))	 as 'server_level', 
+name, compatibility_level
+into #tmp
+from sys.databases
+select 
+cast(server_level as varchar(100)) as server_level,
+cast([name] as varchar(100)) as dbname,
+compatibility_level,
+case  when cast(left(server_level, 2) as int) <> cast(left(compatibility_level, 2) as int) then '' else 'OK' end AS 'diff' 
+from #tmp
+drop table #tmp
+"@
+
+
+$SQL_Get_Database_Files_Query = 
+@"
+SELECT DB_NAME(database_id) AS DatabaseName, name AS LogicalFileName, 
+physical_name AS PhysicalFileName, 
+case file_id 
+	when 2 then 'Log'
+	when 1 then 'Data'
+	else 'Other' 
+end as 'Type'
+FROM sys.master_files
+"@
+
+$SQL_LIST_AG_IPS = 
+@"
+SET NOCOUNT ON
+select l.dns_name, l.port, ip.ip_address, ip_subnet_mask, ip.state_desc, "Data_Center" =   
+      CASE   
+        WHEN ip.ip_address LIKE '10.224.%' THEN 'DCE    - Miami'
+        WHEN ip.ip_address LIKE '172.25%' THEN 'DCE (NON - PROD) - Miami' 
+	    WHEN ip.ip_address LIKE '10.244.%' THEN 'PEAK10 - Invalid' 
+	    WHEN ip.ip_address LIKE '10.56.%'  THEN 'DCC    - Dallas' 
+        ELSE 'UNKNOWN LOCATION - Invalid' 
+      END  
+from sys.availability_group_listener_ip_addresses ip  
+join sys.availability_group_listeners l on l.listener_id = ip.listener_id
+"@
+
+$SQL_Serverlist_AG = "SELECT distinct [Listener Name] + ',' + cast([Port] as varchar(10)) as 'Server' FROM [Master_Application_List].[dbo].[AG SQL Servers]"
---
+++ Master_App_List_version3.ps1
@@ -0,0 +1,446 @@
+﻿<#
+Master_app_List_version3
+#VERSION IN \\ccluatsql1\OPCONJOBS\MasterApplicationList
+made from List_DB_Files.ps1
+Created: 11/19/2018
+Last updated:
+2/12/2019: edited to use real tables not the _XXX
+2/8/2019: added AG Ipaddress check, $GetClusterResources
+2/7/2019: added GetServersAndDBs and Missing backups
+2/6/2019: added get machine type functionality
+2/5/2019: working on adding function calls to insert section, got it with scriptblocks
+1/31/2019: fixed insertion part!
+1/30/2019: working on jobs fixes
+
+Will create report of all database files from all servers from table with servers
+#>
+
+Set-Location I:\OPCONJOBS\MasterApplicationList
+# Imports
+. I:\OPCONJOBS\MasterApplicationList\Master_App_List_SQL.ps1
+<#
+
+    $SQL_Reload_EnvironmentsAndApplications
+    $SQL_GetSQLServers
+    $SQL_GetServers
+    $SQL_Get_DBSFromServer
+    $SQL_GetMissingFrom_ServersAndDatabases
+    $SQL_FixVersionsAndOnlineOffline
+    $SQL_UpdateFrom_EnvironmentsAndApplicationsBACKUP
+    $SQL_GetLogins
+    $SQL_GetUsers
+    $SQL_GetBackupInfo
+    $SQL_ClearMissingBackupsTable
+    $SQL_InsertIntoBackupHistory
+    $SQL_Insert_Production_Values
+    $SQL_Servers_And_Databases_Compatibility_Differences_Query
+    $SQL_Get_Database_Files_Query
+ 	$SQL_LIST_AG_IPS
+	$SQL_Serverlist_AG
+
+#>
+
+
+
+
+
+#===========================functions and scriptblocks=================================================
+<#
+#function Invoke-Sqlcmd3 ($ServerInstance,$Query, $Database)
+function Invoke-Sqlcmd3 ($ServerInstance,$Query)
+# Chad Millers Invoke-Sqlcmd3
+{
+	$QueryTimeout=600
+    $conn=new-object System.Data.SqlClient.SQLConnection
+#	$constring = "Server=" + $ServerInstance + ";Trusted_Connection=True;database=" + $Database
+    $constring = "Server=" + $ServerInstance + ";Trusted_Connection=True"
+	$conn.ConnectionString=$constring
+    $conn.Open()
+	if($conn)
+    {
+    	$cmd=new-object System.Data.SqlClient.SqlCommand($Query,$conn)
+    	$cmd.CommandTimeout=$QueryTimeout
+    	$ds=New-Object System.Data.DataSet
+        $ds
+    	$da=New-Object System.Data.SqlClient.SqlDataAdapter($cmd)
+    	[void]$da.fill($ds)
+    	$conn.Close()
+    	$ds.Tables[0]
+	}
+}
+#>
+
+#Based on Chad Millers Invoke-Sqlcmd3
+$ExecuteSQL = 
+{	
+	param ($ServerInstance, $Query, $Database)
+	$QueryTimeout=600
+	$conn=new-object System.Data.SqlClient.SQLConnection
+	$constring = "Server=" + $ServerInstance + ";Trusted_Connection=True;database=" + $Database
+	$conn.ConnectionString=$constring
+	$conn.Open()
+	if($conn)
+	{
+    	$cmd=new-object System.Data.SqlClient.SqlCommand($Query,$conn)
+    	$cmd.CommandTimeout=$QueryTimeout
+    	$ds=New-Object System.Data.DataSet
+    	$da=New-Object System.Data.SqlClient.SqlDataAdapter($cmd)
+    	[void]$da.fill($ds)
+    	$conn.Close()
+		$ds.Tables[0]
+	}
+}
+
+$Fill_All_Logins =
+{
+	param ($SC, $l, $global:SERVERNAME)
+   	foreach($y in $l)
+    {
+        if ($SC -gt '' -and $y.name -gt '')
+        {
+            $name = $y.name
+		    $dbname = $y.dbname
+		    $sysadmin = $y.sysadmin
+		    $isntgroup = $y.isntgroup
+		    $isntuser = $y.isntuser                
+		    $logininsert = "INSERT INTO [Master_Application_List].[dbo].[All Logins] ([SQLSERVER],[name],[dbname],[sysadmin],[isntgroup],[isntuser]) VALUES "
+		    $logininsert = $logininsert + " ('" + $SC + "','" + $name + "','" + $dbname + "','" + $sysadmin + "','" + $isntgroup + "','" + $isntuser + "')" 
+		    $ErrorActionPreference = "silentlycontinue"
+            $null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $logininsert, "master"))
+        }
+    }
+}
+
+$Fill_All_Users =
+{
+	param ($SC, $usersdb, $global:SERVERNAME)
+   	foreach($uu in $usersdb)
+    {
+        if ($SC -gt '' -and $uu.UserName -gt '')
+        {	
+			$dbname = $uu.DBName
+			$username = $uu.UserName
+			$rolename = $uu.RoleName		
+			$userinsert = "INSERT INTO [Master_Application_List].[dbo].[All Users] ([SQLSERVER],[DB Name],[User Name],[Role Name]) VALUES "
+			$userinsert = $userinsert + " ('" + $SC + "','" + $dbname + "','" + $username + "','" + $rolename + "')"
+			$ErrorActionPreference = "silentlycontinue"
+            $null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $userinsert, "master"))
+		}
+    }   
+}
+
+$Fill_DB_Files = 
+{
+	param ($SC, $d, $global:SERVERNAME)
+   	foreach($y in $d)
+	{
+       	if ($SC -gt '' -and $y.DatabaseName -gt '')
+		{
+			$o = $y.DatabaseName
+			$v = $y.LogicalFileName
+			$h = $y.PhysicalFileName
+			$g = $y.Type
+			$dbinsert = "INSERT INTO [Master_Application_List].[dbo].[Database Files] ([SQLServer],[DatabaseName],[LogicalFileName],[PhysicalFileName],[Type]) VALUES "
+			$dbinsert = $dbinsert + "('" + $SC  + "','" + $o + "','" + $v +  "','" + $h + "','" + $g + "')" 
+			$ErrorActionPreference = "silentlycontinue"
+            $null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $dbinsert, "master"))
+		}
+   	}
+}
+
+$Fill_Server_And_DBs = 
+{
+	param ($SC, $svrdbs, $global:SERVERNAME)
+	foreach ($y in $svrdbs)
+	{
+		$n = $y.name
+		if ($n -gt '')
+		{
+			$o = $y.Online_Offline
+			$v = $y.SqlVersion
+			$dbinsert = "INSERT INTO [Master_Application_List].[dbo].[Servers and Databases] ([SQLServer],[Database_Name],[Online_Offline],[SqlVersion])VALUES"
+			$dbinsert = $dbinsert + "('" + $SC + "','" + $n + "','" + $o + "','" + $v + "')" # + [char]13 + [char]10 + 'GO' + [char]13 + [char]10  THIS IS THE FIX 8/1/2017
+	#		# needed for servers with no access
+			$ErrorActionPreference = "silentlycontinue"
+			$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $dbinsert, "master"))
+		}
+	}	
+}
+
+$Fill_Missing_Backups = 
+{
+	param ($SC, $msbkp, $global:SERVERNAME)
+	foreach ($y in $msbkp)
+	{
+		$db = $y.db
+		if ($db -gt '')
+		{
+			$dt = $y.bkdate
+	#		$svr = [char]39 + [char]34 + $scurr + [char]34 + [char]39
+			$s = "INSERT INTO [Master_Application_List].[dbo].[Missing Backups]([SqlServer],[DBname],[BKDate]) VALUES ('"
+			$s = $s + $SC + "','" + $db + "','" + $dt + "')"
+			$ErrorActionPreference = "silentlycontinue"
+			$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $s, "master"))
+		}
+	}	
+}
+
+
+
+$CheckPing = 
+{
+	param ($server)
+	$v = (ping $server -n 1)
+	foreach ($k in $v)
+	{
+		if ($k.StartsWith("Reply"))
+		{ break }
+		else
+		{ if ($k.StartsWith("Request timed out")) { return "" }	}
+	}
+	$l = $k.Replace('<', '=')
+	$lst = $l.split('=')[2]
+	if ($lst)
+	{ $ping = $lst.Replace('ms TTL', '') }
+	else { $ping = "" }		
+    $p = $ping.Trim()
+    $p2 = [int]$p
+    if ($p -gt "")  
+    {
+        $s = "INSERT INTO [Master_Application_List].[dbo].[SERVERS_LIVE_TODAY] (Machine) VALUES ('" + $server + "')"		
+    }
+    else
+    {
+        $s = "INSERT INTO [Master_Application_List].[dbo].[SERVERS_LIVE_TODAY] (Machine, Status) VALUES ('" + $server+ "', 'DEAD TODAY')"
+    }
+	$s	
+}
+
+
+#Original created by: Jason Wasser
+#Modified: 4/20/2017 03:28:53 PM 
+#https://gallery.technet.microsoft.com/scriptcenter/Get-MachineType-VM-or-ff43f3a9
+#version changed for scriptblock and simplified
+$GetMachineType =
+{
+	param($Computer)
+	$ErrorActionPreference = "silentlycontinue"
+	$Credential = [System.Management.Automation.PSCredential]::Empty
+
+    # Check to see if $Computer resolves DNS lookup successfuly.
+    $null = [System.Net.DNS]::GetHostEntry($Computer)
+   	$ComputerSystemInfo = Get-WmiObject -Class Win32_ComputerSystem -ComputerName $Computer -ErrorAction silentlycontinue -Credential $Credential           
+    switch ($ComputerSystemInfo.Model) 
+	{                  
+        # Check for Hyper-V Machine Type
+        "Virtual Machine" 			{ $MachineType="VM" }
+        # Check for VMware Machine Type
+        "VMware Virtual Platform" 	{ $MachineType="VM" }
+        # Check for Oracle VM Machine Type
+        "VirtualBox" 				{ $MachineType="VM" }
+        # Check for Xen
+        "HVM domU" 					{ $MachineType="VM" }
+        # Otherwise it is a physical Box
+        default 					{ $MachineType="Physical" }
+   	}               
+	$mm = @{}
+    $mm['Type'] = $MachineType 
+	$mm['Manufacturer'] = $ComputerSystemInfo.Manufacturer
+	$mm['Model'] = $ComputerSystemInfo.Model
+	$t = $mm.Type
+	$m = $mm.Manufacturer
+	$md = $mm.Model
+	$dbinsert = "INSERT INTO [Master_Application_List].[dbo].[Machines]([ComputerName],[Type],[Manufacturer],[Model]) VALUES "
+	$dbinsert = $dbinsert + "('" + $Computer + "','" + $t + "','" + $m + "','" + $md + "')"  
+	# this is the return value
+	$dbinsert	
+}
+ 
+$GetClusterResources =
+{
+	param($computerName)
+    $session = New-PSSession -computerName $computername -Authentication Kerberos #-credential $credential 
+    $scriptBlock = 
+    {
+        import-module failoverclusters
+        Get-ClusterResource
+    }
+    
+    Invoke-Command -computerName $computername -scriptBlock $scriptBlock
+    Remove-PSSession -computerName $computername
+} 
+#===========================functions and scriptblocks end=================================================
+
+
+
+
+#======================Start Program============================================
+$global:SERVERNAME = "CCLUATSQL1\UATSQL3"
+$ErrorActionPreference = "silentlycontinue"
+$CLUSTERNAMES = "ccldceshrdcl1", "ccluatdtscl2", "ccluatdtscl4" 
+$SAVESERVER = ""
+$CONCURRENT = 20
+
+# COMMENTED FOR TESTING DO NO DELETE!!!
+
+#Moved this to top of process
+Write-Host "Starting process " 
+Write-Host "Concurrency = " $CONCURRENT.ToString()
+Write-Host "Start time two server tables: " (get-date)
+#=======================Start server section=================================
+# populating table SERVERS_LIVE_TODAY and Machines
+Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, 'truncate table [Master_Application_List].[dbo].[SERVERS_LIVE_TODAY]', "master")
+Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, 'truncate table [Master_Application_List].[dbo].[Machines]', "master")
+$listofservers = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $SQL_GetServers, "master"))
+foreach ($k in $listofservers)
+{
+	if ($k.Machine -gt '')
+	{
+		$running = @(Get-Job | Where-Object { $_.State -eq 'Running' })
+	    if ($running.Count -le $CONCURRENT) 
+	    {
+			$null = Start-Job -Name GetPing    -ScriptBlock $CheckPing  -ArgumentList ($k.Machine) 
+			$null = Start-Job -Name GetMachine -ScriptBlock $GetMachineType -ArgumentList ($k.Machine) 
+		}
+		else
+	    { 
+			$running | Wait-Job 
+		}	
+	    $pngsql 	= (Get-Job -Name GetPing 	| Receive-Job )
+		$null 		= Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $pngsql,  "master")	
+		$machsql 	= (Get-Job -Name GetMachine | Receive-Job )
+		$null 		= Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $machsql, "master")	
+	}
+}	
+		  
+Write-Host "End time two server tables: " (get-date)
+#=======================End of servers=================================
+
+
+#========================Start of sql server tables====================
+$SqlServerList = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $SQL_GetSQLServers, "master"))
+
+Write-Host "Start time five sql server tables: " (get-date)
+# Cleaning destination tables
+Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, "truncate table Master_Application_List.[dbo].[Database Files]", "master")
+Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, "truncate table [Master_Application_List].[dbo].[All Logins]", "master")
+Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, "truncate table [Master_Application_List].[dbo].[All Users]", "master")
+Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, "truncate table [Master_Application_List].[dbo].[Servers and Databases]", "master")
+Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, "truncate table [Master_Application_List].[dbo].[Missing Backups]", "master")
+
+$cnt = 0
+foreach ($x in $SqlServerList)	# ----------Start Outer Server Loop---------------
+{
+    $SC = $x.SqlServer
+    if ($SC -gt '')
+    {#--start of current server---
+	    $scurr = [char]34 + $SC	+ [char]34
+	    $running = @(Get-Job  | Where-Object { $_.State -eq 'Running' })
+	    if ($running.Count -le $CONCURRENT) 
+	    {
+            $null = Start-Job -Name GetDBFiles 			-ScriptBlock $ExecuteSQL -ArgumentList ($scurr, $SQL_Get_Database_Files_Query, "master") 
+#		    $null = Start-Job -Name GetServername 		-ScriptBlock $ExecuteSQL -ArgumentList ($scurr, "select @@servername as SVR", "master") 
+            $null = Start-Job -Name GetAllLogins 		-ScriptBlock $ExecuteSQL -ArgumentList ($scurr, $SQL_GetLogins, "master")
+			$null = Start-Job -Name GetAllUsers 		-ScriptBlock $ExecuteSQL -ArgumentList ($scurr, $SQL_GetUsers, "master") 
+			$null = Start-Job -Name GetServersAndDBs 	-ScriptBlock $ExecuteSQL -ArgumentList ($scurr, $SQL_Get_DBSFromServer, "master") 
+			$null = Start-Job -Name GetMissingBackups 	-ScriptBlock $ExecuteSQL -ArgumentList ($scurr, $SQL_GetBackupInfo, "master") 
+	    } 
+	    else
+	    {
+		    $running | Wait-Job
+	    }	
+	    $d = 				(Get-Job -Name GetDBFiles | Receive-Job ) 
+        $null = 			(Invoke-Command -ScriptBlock $Fill_DB_Files -ArgumentList ($SC, $d, $global:SERVERNAME))
+    
+        $l = 				(Get-Job -Name GetAllLogins | Receive-Job ) 
+		$null = 			(Invoke-Command -ScriptBlock $Fill_All_Logins -ArgumentList ($SC, $l, $global:SERVERNAME))
+
+		$usersdb = 			(Get-Job -Name GetAllUsers | Receive-Job ) 
+		$null = 			(Invoke-Command -ScriptBlock $Fill_All_Users -ArgumentList ($SC, $usersdb, $global:SERVERNAME))
+		
+		$svrdbs = 			(Get-Job -Name GetServersAndDBs | Receive-Job ) 
+		$null = 			(Invoke-Command -ScriptBlock $Fill_Server_And_DBs -ArgumentList ($SC, $svrdbs, $global:SERVERNAME))
+		
+		$msbkp = 			(Get-Job -Name GetMissingBackups | Receive-Job ) 
+		$null = 			(Invoke-Command -ScriptBlock $Fill_Missing_Backups -ArgumentList ($SC, $msbkp, $global:SERVERNAME))
+
+
+#        $w = (Get-Job -Name GetServername | Receive-Job ) 
+#        foreach($e in $w)
+#        {
+#			if ($e.SVR -gt '') { $SAVESERVER = $SAVESERVER + $e.SVR + [char]10 }
+#        }
+		$cnt++
+    }#---end of current server---
+	
+} # ------------------------------------------------End Outer Server Loop---------------
+#Set-content -path c:\temp\svr.txt $SAVESERVER
+Write-Host "Processed " $cnt.ToString() "servers"
+Write-Host "end time five sql server tables: " (get-date)
+#==========================End of sql server tables====================
+
+##### end of COMMENTED FOR TESTING
+
+
+
+
+#==========================Start of misc steps (non jobs)===============
+Write-Host "Start time misc steps " (get-date)
+# Now we need to merge (insert/update) the entries in [Servers and Databases] with
+# the ones in [Environments And Applications]
+$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $SQL_GetMissingFrom_ServersAndDatabases, "master"))
+# Fix #1: SQlVersion is obtained during db expansion, so we update back also onlineoffline
+$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $SQL_FixVersionsAndOnlineOffline, "master"))
+# Update from main storage table [Environments And Applications BACKUP] the expanded lines in [Environments And Applications]
+$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $SQL_UpdateFrom_EnvironmentsAndApplicationsBACKUP, "master"))
+#insert to Missing backups history
+$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $SQL_InsertIntoBackupHistory, "master"))
+#Not doing this now 
+#Final insert from production lines in [Environments And Applications BACKUP]
+#Invoke-Sqlcmd3 $SERVERNAME $SQL_Insert_Production_Values
+#$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $SQL_Insert_Production_Values, "master"))
+Write-Host "End time misc steps " (get-date)
+#==========================End of misc steps (non jobs)=================
+
+
+#==========================Start of AG and clusters=========================
+Write-Host "Start time AG and clusters " (get-date)
+$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, 'truncate table Master_Application_List.[dbo].[AG_IPADDRESS_CHECK]', "master"))
+$SERVERLIST = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $SQL_Serverlist_AG, "master"))
+foreach ($k in $SERVERLIST)
+{
+	$svr = [char]34 + $k.Server + [char]34
+	$j = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($svr, $SQL_LIST_AG_IPS, "master"))
+    foreach ($r in $j)
+    {
+        $s = "INSERT INTO Master_Application_List.[dbo].[AG_IPADDRESS_CHECK]([dns_name],[port],[ip_address],[ip_subnet_mask],[state_desc],[Data_Center]) VALUES "
+        $s = $s + "('" + $r.dns_name + "','" + ,$r.port +  "','" +,$r.ip_address + "','" + $r.ip_subnet_mask + "','" + $r.state_desc + "','" + $r.Data_Center + "')"
+		$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $s, "master"))
+    }
+}
+
+$clusterset = @{}
+#truncate table first
+$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, "truncate table Master_Application_List.[dbo].[CLUSTERS]", "master"))
+
+foreach ($h in $CLUSTERNAMES) # "ccldceshrdcl1", "ccluatdtscl2", "ccluatdtscl4" 
+{
+#   	$clusterset[$h] = GetClusterResources $h
+	$clusterset[$h] = (Invoke-Command -ScriptBlock $GetClusterResources -ArgumentList ($h))
+    foreach ($k in $clusterset[$h])
+    {   
+        if ($k.ToString().Contains(".") -and $k.ToString().Contains("_"))
+        {
+            $m = $k.ToString().Replace("_", "|")
+            $s = $h + '|' + $m
+            $v = $s.Split("|")
+            $sql = "INSERT INTO Master_Application_List.[dbo].[CLUSTERS] ([ClusterMachine],[AG_Group],[IP_ADDRESS]) VALUES "
+            $sql = $sql + "('" + $v[0] + "','" + $v[1]  + "','" + $v[2] + "')"
+			$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($global:SERVERNAME, $sql, "master"))
+        }
+    }
+}
+Write-Host "End time AG and clusters " (get-date)
+Write-Host "==============================================================="
+#==========================end of AG and clusters===========================
+
