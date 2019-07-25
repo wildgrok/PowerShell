a8b14deebfaff1910b5eb5fde18b1915dbffcc70
wildgrok <wildgrok@users.noreply.github.com>
2019-07-25 16:06:54 -0400

    Add files via upload
    Pair version in workstation, uses workflow

--- SQLSMO2_CodeBlocks.ps1
+++ SQLSMO2_CodeBlocks.ps1
@@ -1,7 +1,12 @@
 ﻿#========================================================================
+# SQLSMO2_CodeBlocks.ps1
+#version in C:\Users\jorgebe\Documents\powershell
 # Created with: SAPIEN Technologies, Inc., PowerShell Studio 2012 v3.1.35
 # Created on:   6/5/2019 3:26 PM
 # Created by:   jorgebe
+# used by SQLSMO2.ps1
+# Called from SQLSMO2_WorkFlow.cmd
+# Last modified: 6/24/2019 commented sqlfileheader call (causing timeouts)
 # Organization: 
 # Filename:     
 #========================================================================
@@ -26,6 +31,7 @@ $ExecuteSQL =
 	}
 }
 
+#CHANGED 7/25/2019
 $GetBackupInfo = 
 {
 	param ($server, $backupfile)
@@ -36,7 +42,6 @@ $d = GetBackupInfo $server $bkfile
 foreach ($x in $d)
 {
 	foreach ($y in $x.sqlfilelist)	{$y}
-	foreach ($y in $x.sqlfileheader){$y}
 }
 
 LogicalName          : ActionTags_Data
@@ -143,12 +148,8 @@ ActionTags_Log
 #>
 
 	$dict_backup = @{}
-	$sqlfileheader 	= 'SET NOCOUNT ON RESTORE HEADERONLY FROM DISK = ' + [char]39  + $backupfile + [char]39 
 	$sqlfilelist 	= 'SET NOCOUNT ON RESTORE FILELISTONLY FROM DISK = ' + [char]39 + $backupfile + [char]39
-#	$dict_backup['sqlfilelist'] = Invoke-Sqlcmd3 $server  $sqlfilelist
 	$dict_backup['sqlfilelist'] = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($server, $sqlfilelist, "master"))
-#	$dict_backup['sqlfileheader'] = Invoke-Sqlcmd3 $server  $sqlfileheader
-	$dict_backup['sqlfileheader'] = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($server, $sqlfileheader, "master"))
 	return $dict_backup
 }
 
@@ -175,20 +176,7 @@ $GetLatestBackup =
 			return $k.name
 		}
 	}
-#	return $files
 }
-<#
-#to test
-$backupfolder = '\\Ccldevsql4\devsql2\SQLBACKUPS\SQLBackupUser'
-$filter = '*.*'
-#This brings latest bak file (default):
-(Invoke-Command -ScriptBlock $GetLatestBackup -ArgumentList ($backupfolder))
-#Here we bring latest of each
-(Invoke-Command -ScriptBlock $GetLatestBackup -ArgumentList ($backupfolder, $filter))
-$filter = '*.trn'
-# Here is the latest trn
-(Invoke-Command -ScriptBlock $GetLatestBackup -ArgumentList ($backupfolder, $filter))
-#>
 
 
 $GetBackups = 
@@ -209,17 +197,168 @@ $GetBackups =
 	return $lst2
 }
 
-# to test Sample use:
-#    $backupfolder = '\\CCLDEVSQL4\g$\DEVSQL2\SQLBACKUPS\SQLBackupUser'
-#    This brings all files bak:
-#    $lst = (Invoke-Command -ScriptBlock $GetBackups -ArgumentList ($backupfolder))
-#	$lst
-#    Here we bring a subset using filter string
-#	$filter = 'ZZZ_Deleteme_1*.trn'
-#    $lst = (Invoke-Command -ScriptBlock $GetBackups -ArgumentList ($backupfolder, $filter))
-#	$lst
-#
 
+$RestoreDatabase = 
+{
+	param ($server,$backupfile, $dbname, $logfolder, $datafolder, $restore_options)
+  #needed to import GetLatestBackup
+. C:\Users\jorgebe\Documents\powershell\SQLSMO2_CodeBlocks.ps1	
+<#
+        Restores the database from given backup file to given data and log folders
+        Uses file names based on database name with _Data and _Log suffixes
+		GetBackupInfo returns dictionary with these keys populated
+		$dict_backup['sqlfilelist'] 
+		$dict_backup['sqlfileheader'] 
+		
+		Options for $restore_options['restore_type'] are:
+		DATABASE
+		DIFFERENTIAL
+		If option is LOG returns 
+		options for $restore_options['recovery'] are:
+		RECOVERY
+		NORECOVERY		
+#> 
+
+	# just checking 
+	if ($restore_options['restore_type'] -eq 'LOG') { return }	
+	
+	# if passing a folder get latest backup on it
+	if ($backupfile -like '*.bak')
+	{
+		"Restoring from file"
+		$backupfile2 = $backupfile
+		$backupfile
+	}
+	else
+	{
+		"Latest backup from folder"
+		$bkfile = (Invoke-Command -ScriptBlock $GetLatestBackup -ArgumentList ($backupfile))
+		$backupfile2 = $backupfile + '\' + $bkfile
+		$backupfile2		
+	}
+	
+	$dict_backup = (Invoke-Command -ScriptBlock $GetBackupInfo -ArgumentList ($server, $backupfile2))
+
+	# Start building the restore SQL
+    $s = 'USE MASTER RESTORE DATABASE [' + $dbname + '] FROM DISK =' + [char]39 + $backupfile2 + [char]39 + ' WITH '
+    $filecount = 0
+    $suffix2 = ''
+	#loop for database files
+	foreach( $x in $dict_backup.sqlfilelist)
+	{
+		$ext = [System.IO.Path]::getextension($x.PhysicalName)
+		if ($filecount -gt 0) # after 2 values (0 and 1) we add one to the file name
+		{
+            $suffix2 = $filecount.ToString()
+		}	
+		if ($x.Type -eq 'L')
+		{
+            $suffix = 'Log'
+            $logfilename = $x.logicalname
+			$file_renamed = $dbname + '_' + $suffix + $ext
+			$s = $s + ' MOVE ' + [char]39 + $x.logicalname + [char]39 + ' TO ' + [char]39 + $logfolder + '\' + $file_renamed + [char]39 + ', '
+		}
+        else  # any other type (D,F,S) we put in the Data folder
+		{
+            $suffix = 'Data'
+			$file_renamed = $dbname + '_' + $suffix + $suffix2 + $ext
+			$s = $s + ' MOVE ' + [char]39 + $x.logicalname + [char]39 + ' TO ' + [char]39 + $datafolder + '\' + $file_renamed + [char]39 + ', '
+		}       
+        $filecount = $filecount + 1
+	} # end of database file loop	
+	
+	$s = $s + ' NOUNLOAD, ' + $restore_options['recovery'] + ', STATS = 10'
+    if ($restore_options['replace'] -eq $True)
+	{
+        $s = $s + ', REPLACE;' +  [char]13 + [char]10
+	}	
+		
+    # Doing the actual restore here. NEW: added code to kill connections in the same execution	
+	$s1 =       'DECLARE @kill varchar(8000) = ' + [char]39 + [char]39 + ';'
+    $s1 = $s1 + ' SELECT @kill = @kill + ' + [char]39 + 'kill ' + [char]39 + '  + CONVERT(varchar(5), spid) + ' + [char]39 + ';' + [char]39
+    $s1 = $s1 + ' FROM master..sysprocesses WHERE dbid = db_id(' + [char]39 + $dbname + [char]39 + ') '
+    $s1 = $s1 + ' EXEC(@kill); '
+	$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($server, $s1, "master"))
+	$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($server, $s, "master"))
+	if ($restore_options['recovery'] -eq 'RECOVERY')
+	{
+		$s1 = 'ALTER AUTHORIZATION ON DATABASE::[' + $dbname + '] TO [sa]' +  [char]13 + [char]10
+		$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($server, $s1, $dbname))
+	}	
+	
+	$s = $s + [char]13 + [char]10 + $s1
+	return $s
+}
+
+
+$RestoreLogs = 
+{
+	param ($server,$backupfile, $dbname, $restore_options)
+	#param ($server,$backupfile, $dbname, $logfolder, $datafolder, $restore_options)
+  # Is this needed? this is not calling extra codeblocks
+  # YES IT IS NEEDED - it is calling ExecuteSQL!!!
+. C:\Users\jorgebe\Documents\powershell\SQLSMO2_CodeBlocks.ps1	
+<#
+    Restores the log from given single backup file log or folder with log backups
+	Options for $restore_options['restore_type'] are:
+	DATABASE
+	DIFFERENTIAL
+	LOG
+	If option is not LOG returns 
+	options for $restore_options['recovery'] are:
+	RECOVERY
+	NORECOVERY	
+	Sample
+	RESTORE LOG [ZZZ_Deleteme_4] FROM  DISK = N'G:\DEVSQL2\SQLBACKUPS\SQLBackupUser\ZZZ_deleteme_1_1.trn' 
+	WITH  FILE = 1,  NORECOVERY,  NOUNLOAD,  STATS = 10
+	GO
+	RESTORE LOG [ZZZ_Deleteme_4] FROM  DISK = N'G:\DEVSQL2\SQLBACKUPS\SQLBackupUser\ZZZ_deleteme_1_2.trn' 
+	WITH  FILE = 1,  NOUNLOAD,  STATS = 10
+	GO		
+#> 
+	# just checking 
+	if ($restore_options['restore_type'] -ne 'LOG') { return }	
+	
+	# if passing a folder get latest backup on it
+	if ($backupfile -like '*.trn') #single backup passed
+	{
+		"Restoring from file"
+		$backupfile
+	}
+	else #we are restoring from network folder with several backups
+	{
+#		Get list of backups sorted by time
+#		"Source folder is " $backupfile
+		$bklist = Get-ChildItem  $backupfile  | Sort-Object -Property LastWriteTime -Descending
+		$bklist2 = ""
+		$filter = '*.trn'
+		foreach ($k in $bklist)
+		{
+			if ($k.Name -like $filter)
+			{
+				$bklist2 = $k.Name  + "|" + $bklist2
+			}
+		}
+		#removing last |
+		$bklist2 = $bklist2.Substring(0,$bklist2.Length-1)
+		$lst3 = $bklist2.Split("|")	
+	}
+	
+	# Start building the restore SQL
+	$s = ''
+	foreach ($k in $lst3)
+	{
+		$s = $s + " RESTORE LOG [" + $dbname  + "] FROM  DISK = '" + $backupfile + '\' + $k + "'"
+		$s = $s + " WITH  FILE = 1,  NORECOVERY,  NOUNLOAD,  STATS = 10;" +  [char]13 + [char]10
+	}	
+	if ($restore_options['recovery'] -eq 'RECOVERY')
+	{
+		$s = $s + ' RESTORE DATABASE ' + $dbname + ' WITH RECOVERY;' +  [char]13 + [char]10
+
+	}
+	$null = (Invoke-Command -ScriptBlock $ExecuteSQL -ArgumentList ($server, $s, "master"))
+	return $s
+}
 
 
 
