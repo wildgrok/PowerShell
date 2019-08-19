942abf3c67d44cb7718bca996a5b49a75741482e
wildgrok <wildgrok@users.noreply.github.com>
2019-08-19 16:35:50 -0400

    Add files via upload
    Sample guide for a workflow program

---
+++ Swiss_Army_Knife.ps1
@@ -0,0 +1,40 @@
+﻿
+# Basic_WorkFlow.ps1 - uses Basic_WorkFlow_CodeBlocks.ps1
+
+workflow RunTasks #Sample Swiss Army knife for all your Workflow needs
+ {	
+   param($computers) # we pass a list of computers
+   InlineScript 
+   { 
+      #Execute preliminary actions here
+   }	
+   
+   foreach –parallel ($computer in $computers)
+   {	
+     sequence
+     {
+       InlineScript
+       {
+       
+          # Actions that must occur first
+          
+       }	# end of inlinescript 1
+			
+       InlineScript 
+       {
+          # Actions that must occur later
+									
+       } 	# end of inlinescript 2
+       		
+     }	  # end of sequence		
+   }		  # end of foreach
+	  
+   InlineScript
+   { 	
+    
+          # Closing actions         
+    	
+   } 	    # end of inlinescript 3	
+ }			  # end of workflow
+
+RunTasks -computers 'computer1, computer2, computer3, computer4'
