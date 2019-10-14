add1720a8341a0a13c09ae017d71d14029b7653a
wildgrok <wildgrok@users.noreply.github.com>
2019-10-14 17:34:36 -0400

    Add files via upload

---
+++ Test_Parallel.ps1
@@ -0,0 +1,31 @@
+﻿#========================================================================
+# Created with: SAPIEN Technologies, Inc., PowerShell Studio 2012 v3.1.35
+# Created on:   10/9/2019 5:22 PM
+# Created by:   
+# Organization: 
+# Filename:     
+#========================================================================
+#https://stackoverflow.com/questions/26810722/powershell-workflow-and-parallel-not-giving-output
+
+Workflow Test-Parallel
+{
+	$ServerList = @("Server1","Server2","Server3","Server4","Server5","Server6","Server7","Server8","Server9","Server10")
+	ForEach -Parallel -ThrottleLimit $ServerList.Count ( $Server in $ServerList ) 
+	{
+		Write-Verbose "Starting $Server"
+		$databases = InlineScript{
+			@("db1","db2","db3","db4","db5","db6","db7","db8","db9","db10","db11","db12")
+		}
+		ForEach -Parallel -ThrottleLimit 10 ( $database in $databases ) 
+		{
+			InlineScript{
+				$database = $using:database
+				$Server = $using:Server
+				Write-Verbose "Starting $database on $Server"
+				Start-Sleep -seconds 5
+			}
+		}
+	}
+}
+
+Test-Parallel -Verbose
---
+++ Test_Write_Output.ps1
@@ -0,0 +1,41 @@
+﻿#========================================================================
+# Created with: SAPIEN Technologies, Inc., PowerShell Studio 2012 v3.1.35
+# Created on:   10/14/2019 5:17 PM
+# Created by:   
+# Organization: 
+# Filename:     
+#========================================================================
+
+
+#https://www.reddit.com/r/PowerShell/comments/5ruldk/learning_ps_workflow_output_to_file/
+<#
+Correct, you might be able to get away with writing to the file like that, 
+but chances are you will run into contention. 
+You would want to avoid using a text file to store things that you can easily utilize 
+the pipeline for or store in-memory for performance reasons anyway. 
+Here's a basic example with some of your code commented out:
+#>
+workflow TestCon {
+    param (
+        [string[]]$Computers#, $SiteCode, $HostsFile
+    )
+    #$Computers = Get-Content .\$SiteCode\$HostsFile
+
+    foreach -parallel ($computer in $computers){
+        if (Test-Connection -ComputerName $computer -Count 1 -ErrorAction SilentlyContinue -Quiet){
+            #Out-File .\$SiteCode\$($SiteCode + "Scan.txt") -Append
+            $onlineStatus = $true
+        }
+        else{
+            #Out-File .\$SiteCode\$($SiteCode + "NoPing.txt") -Append
+            $onlineStatus = $false
+        }
+            [pscustomobject]@{'Computer'=$computer;'Online'=$onlineStatus}
+    }
+}
+
+$resultList = New-Object System.Collections.Generic.List[object]
+
+TestCon -Computers @("localhost","server1","server2","fakeComputer","localhost","server3") | ForEach-Object {$resultList.Add($_); $_ } | Where-Object {$_.Online } | ForEach-Object {"I should do something with $($_.Computer)"}
+
+$resultList | Select Computer, Online
