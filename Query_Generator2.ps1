2223de8a71fb2a330e48d864e78cb1b735322319
wildgrok <wildgrok@users.noreply.github.com>
2019-10-17 17:01:34 -0400

    Add files via upload
    Generates change tracking select queries for all change tracking enabled tables in a database

--- Query_Generator2.ps1
+++ Query_Generator2.ps1
@@ -1,33 +1,22 @@
-﻿<#
-Query_Generator2.ps1
-version in 
-C:\Users\jorgebe\Documents\genesis\change_tracking_genesis
-
-Created:        5/22/2018 from original 4/19/2018
-Last Modified:
-6/15/2018 big changes fixed detection of multiple rows
-6/14/2018 fixed issue of missing last column
-6/12/2018 fixed all, it creates procs for delta and table creation
-5/22/2018 no need for mapping.txt
+﻿
+#﻿Query_Generator2.ps1
+#version in 
+#C:\Users\jorgebe\Documents\powershell
+#Last Modified:
+#10/17/2019: fixed issue with $SQL_tables_column query
+#10/16/2019: changed for select only, no procs 
 
 
-Creates set of queries for change tracking
-- Scripts for table creation
-- Script for proc creation (to be scheduled with desired frequency)
-
-
-
-#>
 
 #-------globals-----------------------------
 #$path = 'C:\Users\jorgebe\Documents\genesis\change_tracking_genesis\deployment'
 #$path = 'C:\Users\jorgebe\Documents\genesis\JackpotBlitz\Data Strategy Data Transfer Service\DEPLOYMENT'
-$path = 'C:\Users\jorgebe\Documents\Data Sync Project - KT Srini\CHANGE_TRACKING_VERSION'
+$path = 'C:\Users\jorgebe\Documents\powershell'
 
 Set-Location $path
-$CURRENT_DATABASE = "Offer_Change_Tracking"
-$WEBMETHODS_DATABASE = "Offer_Change_Tracking"
-$CURRENT_SERVER   = 'Ccluatshrddb1\uatsql2'
+$CURRENT_DATABASE = "TableGames"
+$WEBMETHODS_DATABASE = "TableGames"
+$CURRENT_SERVER   = 'CCLDEVSBXDB1\CCLDEVSBXDB1'
 #$CURRENT_DATABASE = "Genesis_Breeze"
 #$WEBMETHODS_DATABASE = "WM_AUDIT"
 #$CURRENT_SERVER   = 'XXDEVSQL3'
@@ -35,9 +24,12 @@ $CURRENT_SERVER   = 'Ccluatshrddb1\uatsql2'
 
 $CRLF = [char]13 + [char]10
 
-$s = "set nocount on select  b.name as 'table', a.name as 'column' from " + $CURRENT_DATABASE + ".sys.columns a "
-$s = $s + "	join " + $CURRENT_DATABASE + ".sys.tables b on a.object_id = b.object_id join " + $CURRENT_DATABASE + ".sys.change_tracking_tables c on "
-$SQL_tables_column = $s + "	c.object_id = b.object_id"
+$s = "select  (s.name + '.' + t.name) as 'table', a.name as 'column' from " + $CURRENT_DATABASE + ".sys.columns a "
+$s = $s + "join " + $CURRENT_DATABASE +  ".sys.tables t on 	a.object_id = t.object_id "
+$s = $s + "join " + $CURRENT_DATABASE + ".sys.change_tracking_tables c on c.object_id = t.object_id "
+$s = $s + "JOIN " + $CURRENT_DATABASE + ".sys.schemas s ON t.[schema_id] = s.[schema_id]"
+$SQL_tables_column = $s
+
 
 #-------functions---------------------------
 function Invoke-Sqlcmd3 ($ServerInstance, $Query)
@@ -70,52 +62,11 @@ function BuildScriptsOneTable ($database, $table, $colarray)
         Write-Host 'wm database ' $WEBMETHODS_DATABASE
         Write-Host 'table ' $table
         Write-Host 'columns ' $colarray
-
-        # Build the sql for initial data load---------------
-        # CHANGED to create tables with null
-        
-        $s = 'SET NOCOUNT ON' + $CRLF
-        $s = $s + 'USE [' +  $WEBMETHODS_DATABASE + ']' + $CRLF
-        $s = $s + 'if exists (select name from sys.tables where name = ' + [char]39 + $CURRENT_DATABASE + '_' + $table + [char]39 + ')'  + $CRLF
-        $s = $s + "drop table " + $CURRENT_DATABASE + '_' + $table + $CRLF
-        $s = $s + 'CREATE TABLE ' + $CURRENT_DATABASE + '_' + $table + '('  + $CRLF       
-        $cnt = 0
-        # ----columns loop start-------------------
-        foreach ($k in $colarray)
-        {
-#            Write-Host "col:" $k $cnt.ToString()
-            if ($cnt -lt $colarray.Count)
-            {
-                $s = $s + $k + ' varchar(8000) null ,' + $CRLF               
-            }
-           $cnt++
-        }
-        $s = $s + 'SYS_CHANGE_OPERATION varchar(8000) null,'  + $CRLF
-#        $s = $s + 'SYS_CHANGE_CONTEXT varchar(8000) null ,' + $CRLF
-        $s = $s + 'LAST_SYNCH bigint null )' + $CRLF
-        # -----columns loop end------
-        Write-Host $s
-        Set-Content ($CURRENT_DATABASE + '_' + $table + '_' + 'table_creation.sql') $s
-        # --- end of initial data load--------------------------------------
-        
-        
         # Now build the sql for deltas--------------------------------------
         $s = 'SET NOCOUNT ON' + $CRLF
-        $s = $s + 'USE [' + $CURRENT_DATABASE + ']' + $CRLF
-        $s = $s + 'if exists (select name from sysobjects where name = ' + [char]39 + 'USP_DELTA_' + $table + [char]39 + ')'  + $CRLF
-        $s = $s + 'drop proc USP_DELTA_' + $table + $CRLF
-        $s = $s + 'GO' + $CRLF
-        $s = $s + 'CREATE PROC [USP_DELTA_' + $table + ']' + $CRLF
-        $s = $s + 'as ' + $CRLF
-        $s = $s + 'SET NOCOUNT ON' + $CRLF        
+        $s = $s + 'GO' + $CRLF    
         $s = $s + 'declare @synchid bigint' + $CRLF
-        $s = $s + 'declare @max_synch bigint' + $CRLF
-        $s = $s + 'set @synchid = CHANGE_TRACKING_CURRENT_VERSION();' + $CRLF
-        $s = $s + 'select @max_synch = max([LAST_SYNCH]) from ' + $WEBMETHODS_DATABASE + '.dbo.' + $CURRENT_DATABASE + '_' + $table + $CRLF
-        $s = $s + 'if (@max_synch is null) set @max_synch = -1' + $CRLF
-#        $s = $s + 'if (@synchid > @max_synch) ' + $CRLF
-#        $s = $s + 'begin' + $CRLF
-        $s = $s + 'INSERT INTO ' + $WEBMETHODS_DATABASE + '.dbo.' + $CURRENT_DATABASE + '_' + $table + $CRLF   
+        $s = $s + 'set @synchid = CHANGE_TRACKING_CURRENT_VERSION() -1;' + $CRLF
         $s = $s + "SELECT " + $CRLF
         $cnt = 0
         # ----columns loop start-------------------
@@ -134,16 +85,14 @@ function BuildScriptsOneTable ($database, $table, $colarray)
             }
             $cnt++
         }
-        $s = $s + 'cast(CT.SYS_CHANGE_OPERATION as varchar(8000)) as ' +[char]39 + 'SYS_CHANGE_OPERATION' + [char]39 + ',' +$CRLF
+        $s = $s + 'cast(CT.SYS_CHANGE_OPERATION as varchar(8000)) as ' +[char]39 + 'SYS_CHANGE_OPERATION' + [char]39 + ', ' +$CRLF
 #        $s = $s + 'cast(CT.SYS_CHANGE_CONTEXT as varchar(8000)) as ' +[char]39 + 'SYS_CHANGE_CONTEXT' + [char]39 + ',' + $CRLF
         $s = $s + '@synchid as ' + [char]39 + 'LAST_SYNCH' + [char]39 + $CRLF       
         # -----columns loop end------
-        $s = $s + ' FROM [dbo].[' + $table + '] AS p'  + $CRLF
+        $s = $s + ' FROM ' + $table + ' AS p'  + $CRLF
         $s = $s + 'RIGHT OUTER JOIN ' + $CRLF
-        $s = $s + 'CHANGETABLE(CHANGES [dbo].[' + $table + '], @max_synch) AS CT ON' + $CRLF
-        $s = $s + 'P.[' + $colarray[0] + '] = CT.[' + $colarray[0] + ']' + $CRLF
-        $s = $s + 'WHERE @synchid > @max_synch' +  $CRLF
-#        $s = $s + 'end' + $CRLF
+        $s = $s + 'CHANGETABLE(CHANGES ' + $table + ', @synchid) AS CT ON ' + $CRLF
+        $s = $s + 'P.' + $colarray[0] + ' = CT.' + $colarray[0] + ' ' + $CRLF
         Write-Host $s
         Set-Content ($CURRENT_DATABASE + '_' + $table + '_' + 'DELTA_DATA.sql') $s     
         # -- end of deltas--------------------------------------------------      
@@ -151,7 +100,7 @@ function BuildScriptsOneTable ($database, $table, $colarray)
 
 function CreateDict
 {
-    $p = Invoke-Sqlcmd3 $CURRENT_SERVER $SQL_tables_column
+    $p = Invoke-Sqlcmd3 ([char]34 + $CURRENT_SERVER + [char]34) $SQL_tables_column
     $dict = @{}
     $alreadyprocessed = ""
     foreach ($k in $p)
@@ -166,6 +115,7 @@ function CreateDict
                         if (!$dict[$k.table])
                         {
                             $dict[$k.table] = $r.column
+#                            $dict[$k.table] = $r.table
                         }
                         else
                         {
